de0f6ed59d4fda7ff5320405367ee3be19b7314b
neglashkari <109758200+neglashkari@users.noreply.github.com>
2022-10-31 22:04:21 -0400

    Update pid_controller.cpp

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -19,6 +19,23 @@ void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, doubl
    /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    **/
+  
+  
+ //defining coefficients
+   kp = Kpi;
+   kd = Kdi;
+   ki = Kii;
+  
+   //initializing errors
+   cte_prev = 0.0; //error
+   cte_der = 0.0; //derivative of error
+   cte_int = 0.0; //integral of error
+   dt = 0.0;
+   
+   //define limits
+   max_lim = output_lim_maxi;
+   min_lim = output_lim_mini; 
+  
 }
 
 
@@ -26,6 +43,11 @@ void PID::UpdateError(double cte) {
    /**
    * TODO: Update PID errors based on cte.
    **/
+  
+   cte_prev = cte;
+   cte_der = dt > 0.0 ? (cte - cte_prev) / dt : 0.0; 
+   cte_int += cte * dt;
+  
 }
 
 double PID::TotalError() {
@@ -33,12 +55,25 @@ double PID::TotalError() {
    * TODO: Calculate and return the total error
     * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
    */
-    double control;
-    return control;
+  
+
+   double control = kp*cte_prev + kd*cte_der + ki*cte_int;
+	
+   if (control > max_lim) {
+      control = max_lim;
+   } else if (control < min_lim) {
+      control = min_lim;
+   }
+
+   return control;
 }
 
 double PID::UpdateDeltaTime(double new_delta_time) {
    /**
    * TODO: Update the delta time with new value
    */
+  
+  dt = new_delta_time;
+  
+  return dt;
 }
